d4ab46c0eab134423a61c3dd8e88a047c6d52bc0
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 13:23:57 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0 for success value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+
+	return (0);
+}
