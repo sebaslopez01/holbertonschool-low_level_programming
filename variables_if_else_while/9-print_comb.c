2fe46c597092bc6bb9579ba8e581c4218023eb5a
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 13:45:50 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- combinations
+ *
+ * Return: 0 for success value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
