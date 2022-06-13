9a219e774d6166f1a588aaf41c3f4a19a6908f8b
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 10:25:33 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)", sizeof(char));
 	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %ld byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
 	printf("Size of a float: %f byte(s)", sizeof(float));
 	return (0);
 }
