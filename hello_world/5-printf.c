4fb38115203d523c22cde47e6b6cc38bd0906098
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 10:19:08 -0700

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints text with printf function
+ *
+ * Return: 0 for success value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
