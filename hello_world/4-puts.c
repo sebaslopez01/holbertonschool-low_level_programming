f7cfb83f1ae20b6d144d59a87f0221cc77b60060
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 10:16:21 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints text to standar output
+ *
+ * Return: 0 if all is Ok
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
