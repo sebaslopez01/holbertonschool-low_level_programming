ca6cf0f061bac6aedee151e6871fb9676c0fc3f8
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 13:29:11 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - numbers
+ *
+ * Return: 0 for success value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
