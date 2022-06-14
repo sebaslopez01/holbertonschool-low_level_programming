21bea3f83d3ceb4ae52405ac0b7b6d9d4714e183
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 19:50:49 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int c;
+	int res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			res = i * c;
+
+			if (res < 10)
+			{
+				_putchar('0' + res);
+			}
+			else
+			{
+				_putchar('0' + res / 10);
+				_putchar('0' + res % 10);
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
