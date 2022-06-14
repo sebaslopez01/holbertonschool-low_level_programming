7c58f412743c7f715a9928211476f25d88a9bd01
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 19:00:28 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ * @n: Digit to filter
+ *
+ * Return: Last digit of n
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	_putchar(l);
+
+	return (l);
+}
