137f83c7484f3f51381776a814d03d242300361b
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 18:27:35 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
