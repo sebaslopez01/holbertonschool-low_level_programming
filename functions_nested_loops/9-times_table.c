f2a19029e14a8b3d0b98e54249491a592883eeaf
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 20:00:14 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,26 +10,34 @@ void times_table(void)
 	int i;
 	int c;
 	int res;
+	int next;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c < 10; c++)
 		{
 			res = i * c;
+			next = i * (c + 1);
 
 			if (res < 10)
 			{
 				_putchar('0' + res);
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 			else
 			{
 				_putchar('0' + res / 10);
 				_putchar('0' + res % 10);
+			}
+
+			if (c != 9)
+			{
 				_putchar(',');
 				_putchar(' ');
+
+				if (next < 10)
+				{
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
