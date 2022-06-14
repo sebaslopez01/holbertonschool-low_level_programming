de460c9ffeb842c150d16fd8c7857e4b72d2e8ae
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 19:52:54 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,14 +20,17 @@ void times_table(void)
 			if (res < 10)
 			{
 				_putchar('0' + res);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar('0' + res / 10);
 				_putchar('0' + res % 10);
+				_putchar(',');
+				_putchar(' ');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
