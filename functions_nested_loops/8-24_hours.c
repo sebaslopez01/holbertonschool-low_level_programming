c21cf671baeefe6d4a066fb010735d53c69d2c6b
Sebastian <sebaslopez.2002@gmail.com>
2022-06-13 19:39:19 -0700

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+		}
+	}
+}
