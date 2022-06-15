f045e2377c33e61c85aa877d3319935715c52955
Sebastian <sebaslopez.2002@gmail.com>
2022-06-15 15:33:35 -0700

    101

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - print 3
+ *
+ * Return: 0
+ */
+void main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			for (k = 50; j <= 58; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i != 7 && j != 8 && k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+}
