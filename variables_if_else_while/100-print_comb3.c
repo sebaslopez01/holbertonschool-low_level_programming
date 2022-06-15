46f16c158e1cbcc11b6d6d617d3f2d4bf8c75328
Sebastian <sebaslopez.2002@gmail.com>
2022-06-15 09:23:56 -0700

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,25 +9,22 @@ int main(void)
 {
 	int i;
 	int j;
-	int t;
-
-	t = 0;
 
 	for (i = 48; i <= 56; i++)
 	{
 		for (j = 49; j <= 57; j++)
-		{		
-			putchar(i);
-			if (j == 49)
-				putchar(j + t);
-			else
+		{
+			if (j > i)
+			{
+				putchar(i);
 				putchar(j);
-			if (i == 56 && j == 57)
-				break;				
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
+
+				if (i == 56 && j == 57)
+					break;
+			}
 		}
-		t++;
 	}
 	putchar('\n');
 
