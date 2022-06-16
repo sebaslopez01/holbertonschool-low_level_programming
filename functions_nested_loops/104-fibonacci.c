6dde02cc69c0169b01eeab657a42c7b8eb9fe4dc
Sebastian <sebaslopez.2002@gmail.com>
2022-06-16 09:21:35 -0700

    104

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - 98 fibo
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	unsigned int long last = 0;
+	unsigned int long current = 1;
+
+	for (i = 0; i < 97; i++)
+	{
+		unsigned int long res = current + last;
+
+		printf("%lu", res);
+
+		unsigned int long temp = current;
+
+		current = res;
+
+		last = temp;
+
+		if (i != 96)
+			printf(", ");
+	}
+	putchar('\n');
+
+	return (0);
+}
