89bbe925f026cafd17b889054c8524f0cec7fab3
Sebastian <sebaslopez.2002@gmail.com>
2022-06-16 06:57:28 -0700

    100

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table
+ * @n: Number of times table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		_putchar('\n');
+		return;
+
+	int i;
+	int j;
+	int res;
+	int next;
+
+	for (i = 0; i < n + 1; i++)
+	{
+		for (j = 0; j < n + 1; j++)
+		{
+			res = i * j;
+			next = i * (j + 1);
+
+			print_chars('0' + res);
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (n >= 10 && next < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (n < 10 && next < 10)
+					_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -23,3 +23,5 @@ int add(int a, int b);
 void print_to_98(int n);
 
 void print_chars(int n);
+
+void print_times_table(int n);
