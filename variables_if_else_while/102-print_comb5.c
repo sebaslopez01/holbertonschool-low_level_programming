5f81762c0170be0f69d56babffa987bb6d0d59ff
Sebastian <sebaslopez.2002@gmail.com>
2022-06-17 21:22:45 -0700

    102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - comb 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i != 98 && j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
