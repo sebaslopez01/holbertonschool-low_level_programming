c6184c4c8891db900a17beacf393cb394fe558be
Sebastian <sebaslopez.2002@gmail.com>
2022-06-20 14:54:55 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line
+ * @n: Number of times the character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
