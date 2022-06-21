3d91aaabdc92a2e1feeb675adb9b5a704884b7ae
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 09:52:48 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - Copies a string
+ * @dest: Buffer to copy
+ * @src: Source to cpy the string
+ *
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	int len = 0;
+
+	while (src[len])
+		len++;
+
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
