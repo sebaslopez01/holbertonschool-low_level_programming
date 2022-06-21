ba20f8c859744a01a003aefba456b6ec557116a5
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 11:02:19 -0700

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -24,18 +24,12 @@ int lenstr(char *s)
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	char new_s[400];
 
 	int len1 = lenstr(dest);
 	int len2 = lenstr(src);
 
-	for (i = 0; i < len1; i++)
-		new_s[i] = dest[i];
+	for (i = 0; i <= len2; i++)
+		new_s[len1 + i] = src[i];
 
-	for (i = 0; i < len2; i++)
-		new_s[i + len1 - 1] = src[i];
-
-	new_s[len1 + len2] = '\0';
-
-	return (*new_s);
+	return (dest);
 }
