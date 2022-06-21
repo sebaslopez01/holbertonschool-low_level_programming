37132fbeea85f2fdc14d68a421147d74bdd0a303
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 10:54:10 -0700

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,41 @@
+
+/**
+ * lenstr - Get length of string
+ * @s: String
+ *
+ * Return: length of string
+ */
+int lenstr(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Dest string
+ * @src: Source String
+ *
+ * Return: A pointer to resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char new_s[400];
+
+	int len1 = lenstr(dest);
+	int len2 = lenstr(src);
+
+	for (i = 0; i < len1; i++)
+		new_s[i] = dest[i];
+
+	for (i = 0; i < len2; i++)
+		new_s[i + len1 - 1] = src[i];
+
+	new_s[len1 + len2] = '\0';
+
+	return (dest);
+}
