1f860bdb0e97d6325173d8ef273acef58f448606
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 13:07:44 -0700

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	int len = 0;
-
-	while (dest[len])
-		len++;
-
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	
-	dest[len + n] = '\0';
+
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
