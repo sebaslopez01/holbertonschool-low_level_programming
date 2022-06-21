8411904b3bb951dfba4f92a1a6266d9d4521a260
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 09:25:24 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: String
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int len = 0;
+
+	while (str[len])
+		len++;
+
+	for (i = (len / 2) - 1; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
