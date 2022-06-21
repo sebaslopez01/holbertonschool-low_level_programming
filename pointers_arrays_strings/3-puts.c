54ffa564052c5c6a1abcb89ca40be05e93dcc3be
Sebastian <sebaslopez.2002@gmail.com>
2022-06-20 20:27:48 -0700

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: String to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (str[i] == 0)
+			break;
+		_putchar(str[i]);
+
+		i++;
+	}
+}
