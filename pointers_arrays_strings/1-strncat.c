fe157e861ffa6f929d9b48f69413bf79ee661cc1
Sebastian <sebaslopez.2002@gmail.com>
2022-06-21 12:51:47 -0700

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+/**
+ * lenstr - String length
+ * @s: String
+ *
+ * Return: String length
+ */
+int lenstr(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: Dest string
+ * @src: Source String
+ * @n: Use at most n bytes
+ *
+ * Return: Pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	int len1 = lenstr(dest);
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
