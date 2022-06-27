96e17e085dd32f907efc04e032f1e6d0cd1d398e
Sebastian <sebaslopez.2002@gmail.com>
2022-06-27 16:09:16 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] < s2[i])
-			return (-1);
+		s1++;
+		s2++;
 	}
 
-	return (0);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
 }
