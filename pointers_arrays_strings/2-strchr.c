b12ed8f3d422909d9965aada03a375f8fab544b0
Sebastian <sebaslopez.2002@gmail.com>
2022-06-27 22:26:31 -0700

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	char *p = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			p = &s[i];
+	}
+
+	return (p);
+}
