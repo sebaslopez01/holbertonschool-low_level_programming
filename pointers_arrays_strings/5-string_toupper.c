e8e38586b3cba207709c49a177280ae4b7dc5997
Sebastian <sebaslopez.2002@gmail.com>
2022-06-27 17:06:12 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase
+ * @s: String
+ *
+ * Return: String
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
