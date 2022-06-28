660981291aa189e77a008878e29352a2346a4d6d
Sebastian <sebaslopez.2002@gmail.com>
2022-06-27 17:25:18 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+/**
+ * is_separator - Checks if is separator
+ * @c: Character
+ *
+ * Return: 1 for Yes or 0 for No
+ */
+int is_separator(char c)
+{
+	switch (c)
+	{
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			return (1);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: String
+ *
+ * Return: String
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (is_separator(s[i - 1]) && (s[i] >= 97 && s[i] <= 122))
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
