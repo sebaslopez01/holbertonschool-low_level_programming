bce7e40db6adca246ad887b8ce5ea3c79b255646
Sebastian <sebaslopez.2002@gmail.com>
2022-06-29 10:28:29 -0700

    101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,15 @@
  */
 void print_number(int n)
 {
-	_putchar('0' + n);	
+	int i;
+	int len;
+	char str[20];
+
+	sprintf(str, "%d", n);
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
