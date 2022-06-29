690c7a9a21494eae016da5fd8f76a61fdf4feebf
Sebastian <sebaslopez.2002@gmail.com>
2022-06-29 10:19:10 -0700

    101

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the largest prime factor of the number 612852475143
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+				
+
+	return (0);
+}
---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: Integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	
+}
