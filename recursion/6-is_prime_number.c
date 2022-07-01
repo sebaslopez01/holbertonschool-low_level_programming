7d29dfd65ad40ece0bc16873d5df6c88a1425379
Sebastian <sebaslopez.2002@gmail.com>
2022-06-30 20:32:50 -0700

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+/**
+ * helper_f - Helps to get prime number
+ * @c: Counter
+ * @n: Number to verify
+ *
+ * Return: 1 if is a prime number, 0 otherwise
+ */
+int is_prime(int c, int n)
+{
+	if (n % c == 0)
+		return (0);
+	if (c <= n / 2)
+		is_prime(c + 1, n);
+	else
+		return (1);
+}
+
+/**
+ * is_prime_number - Verify that the input is a prime number
+ * @n: Number to verify
+ *
+ * Return: 1 if is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	if (n == 1)
+		return (0);
+	return (is_prime(1, n));
+}
