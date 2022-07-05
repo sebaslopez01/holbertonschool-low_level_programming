7c70788e4c779e774e87c94598d10ac49ea4237b
Sebastian <sebaslopez.2002@gmail.com>
2022-07-05 16:55:18 -0700

    1

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives
+ * @argc: Size of argv
+ * @argv: Array of arguments
+ *
+ * Return: 0 for success, 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
