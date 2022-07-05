1667175d5952eb7fe00f8417aad00f05bfcdf872
Sebastian <sebaslopez.2002@gmail.com>
2022-07-05 12:23:15 -0700

    0

---
+++ testing/string_concat2.c
@@ -0,0 +1,65 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: n bytes to take from s2
+ *
+ * Return: Pointer to a newly allocated space in memory with concatenated strings
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3 = NULL;
+
+	int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	unsigned int len1 = strlen(s1), len2 = strlen(s2), n2 = n;
+
+	if (n >= len2)
+		n2 = len2;
+
+	s3 = malloc(sizeof(char) * (len1 + n2));
+
+	if (!s3)
+		return (NULL);
+
+	while (i != len1 + n2)
+	{
+		if (i < len1)
+			s3[i] = s1[i];
+		else
+		{
+			s3[i] = s2[j];
+			j++;
+		}
+
+		i++;
+	}
+
+	s3[len1 + n2] = '\0';
+
+	return (s3);
+}
+
+
+int main(void)
+{
+	char *s1 = "Best ";
+	char *s2 = "School !!!";
+
+	char *s3 = string_nconcat(s1, NULL, 6);
+
+	printf("%s\n", s3);
+
+	free(s3);
+
+	return (0);
+}
