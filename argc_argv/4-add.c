331e15939bd7e2b154839b505c9871c763e48891
Sebastian <sebaslopez.2002@gmail.com>
2022-07-06 09:58:26 -0700

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	{
 		for (j = 0; j < strlen(argv[i]); j++)
 		{
-			if (argv[i][j] < 48 && argv[i][j] > 57)
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 
