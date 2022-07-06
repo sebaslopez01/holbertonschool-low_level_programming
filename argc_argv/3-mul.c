d14abdba4658aa664d60c3f464b7e2b3d441751c
Sebastian <sebaslopez.2002@gmail.com>
2022-07-05 17:04:50 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,6 +10,8 @@
  */
 int main(int argc, char **argv)
 {
+	int n1, n2;
+
 	if (argc <= 2 && argc >= 4)
 	{
 		printf("Error\n");
@@ -17,8 +19,8 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	int n1 = atoi(argv[1]);
-	int n2 = atoi(argv[2]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
 
 	printf("%d\n", n1 + n2);
 
