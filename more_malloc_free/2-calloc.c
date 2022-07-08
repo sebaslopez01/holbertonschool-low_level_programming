dbc276489c6d525cffe158db4f875fde5e58f07e
Sebastian <sebaslopez.2002@gmail.com>
2022-07-08 09:10:44 -0700

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,6 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i;
+
 	void *p = NULL;
 
 	if (nmemb == 0 || size == 0)
@@ -18,6 +20,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!p)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = 0;
 
 	return (p);
 }
