3aa688e111a652b0854b4db58108b100da5099c5
Sebastian <sebaslopez.2002@gmail.com>
2022-07-10 19:44:39 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -24,25 +24,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	if (strcpy(d->name, name) == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
+	strcpy(d->name, name);
 
 	d->age = age;
 
 	d->owner = malloc(strlen(owner) + 1);
 	if (d->owner == NULL)
 	{
+		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	if (strcpy(d->owner, owner) == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
+	strcpy(d->owner, owner);
 
 	return (d);
 }
