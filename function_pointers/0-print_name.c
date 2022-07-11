712c9436b180360e0cce98076d5db42e31859b48
Sebastian <sebaslopez.2002@gmail.com>
2022-07-11 06:48:03 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - Prints a name
  * @name: Name to print
@@ -7,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
