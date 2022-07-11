1e1b597a2a99d867e1df28acb2650a194a65675c
Sebastian <sebaslopez.2002@gmail.com>
2022-07-10 18:33:04 -0700

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - Initialize a dog struct
+ * @d: Dog Struct Pointer
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
