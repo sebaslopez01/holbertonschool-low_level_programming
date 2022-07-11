d9a9aa6aa4434e8bd5d0bf6f3258ce8900b7a3c4
Sebastian <sebaslopez.2002@gmail.com>
2022-07-10 19:25:36 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - Creates a new dog struct
@@ -18,9 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	strcpy(d->owner, owner);
 
 	return (d);
 }
