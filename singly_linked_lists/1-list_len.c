709963696e9befa4b75e3f2a40703e7c2ab820ca
Sebastian <sebaslopez.2002@gmail.com>
2022-07-25 18:45:54 -0700

    1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * list_len - Count the number of elements in a linked list_t list
+ * @h: list_t list
+ *
+ * Return: Number of elements of a list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
