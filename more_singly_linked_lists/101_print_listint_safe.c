4d3bf13d85b89f021d71e36606026d6b882c576b
Sebastian <sebaslopez.2002@gmail.com>
2022-07-28 09:07:00 -0700

    101

---
+++ more_singly_linked_lists/101_print_listint_safe.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", head->next, head->n);
+		count++;
+		head = head->next;
+	}
+
+	return (count);
+}
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -39,6 +39,8 @@ int sum_listint(listint_t *head);
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
+size_t print_listint_safe(const listint_t *head);
+
 size_t free_listint_safe(listint_t **h);
 
 #endif /* LISTS_H */
