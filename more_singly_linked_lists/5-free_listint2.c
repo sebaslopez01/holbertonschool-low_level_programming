746a3fd6f1e8dfad14a0d3a9a64d8520f711a622
Sebastian <sebaslopez.2002@gmail.com>
2022-07-28 07:49:17 -0700

    5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: listint_t list to free
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = NULL;
+
+	while (*head != NULL)
+	{
+		current = *head;
+
+		*head = (*head)->next;
+
+		free(current);
+	}
+}
