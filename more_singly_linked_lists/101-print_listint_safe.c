d4e78b9867ae011b860bb8cbdcde8712a1689953
Sebastian <sebaslopez.2002@gmail.com>
2022-07-28 12:11:48 -0700

    101

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -4,11 +4,20 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
+	const listint_t *head_copy = head;
 
 	while (head != NULL)
 	{
+		if (head_copy->next != NULL && head_copy->next->next != NULL)
+			head_copy = head_copy->next->next;
+
 		printf("[%p] %d\n", (void *)head, head->n);
+
+		if (head->next == head_copy)
+			exit(98);
+
 		count++;
+
 		head = head->next;
 	}
 
