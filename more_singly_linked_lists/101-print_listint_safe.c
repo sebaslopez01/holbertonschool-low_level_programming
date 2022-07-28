edfdc0e909c622128de49114ba77d45d4a4defba
Sebastian <sebaslopez.2002@gmail.com>
2022-07-28 09:19:41 -0700

    101

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -4,12 +4,13 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
+	listint_t *current_node = head;
 
-	while (head != NULL)
+	while (current_node != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p] %d\n", current_node, current_node->n);
 		count++;
-		head = head->next;
+		current_node = current_node->next;
 	}
 
 	return (count);
