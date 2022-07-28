dd297cadeca198810a621004f682639b30767ea1
Sebastian <sebaslopez.2002@gmail.com>
2022-07-27 18:05:26 -0700

    0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - Prints all elements of a listint_t list
+ * @h: listint_t list to print
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
