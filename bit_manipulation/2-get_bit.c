6f9f6754fca7ed3d714ad22ccb5858b488036ba4
Sebastian <sebaslopez.2002@gmail.com>
2022-08-02 07:42:47 -0700

    2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int n_copy = n, pos_binary = 1;
 	unsigned int num_digits;
 
-	for (num_digits = 0; n_copy > 0; num_digits++)
+	for (num_digits = 0; n_copy > 1; num_digits++)
 		n_copy >>= 1;
 
 	if (index > num_digits)
