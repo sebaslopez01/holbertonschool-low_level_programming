519f7cb0abcf0c542c1be42ff376992a35395e4a
Sebastian <sebaslopez.2002@gmail.com>
2022-08-03 09:28:45 -0700

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	bytes_written = write(fd, text_content, strlen(text_content));
-	if (bytes_wirtten == -1)
+	if (bytes_written == -1)
 		return (-1);
 
 	return (1);
