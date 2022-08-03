887535b6c2124dd22e369fc23c59244aa6c2db1d
Sebastian <sebaslopez.2002@gmail.com>
2022-08-03 14:28:14 -0700

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * dlistint_len - Count the number of elements in a linked dlistint_t list
+ * @h: dlistint_t list to count
+ *
+ * Return: The number of elements of the dlistint_t list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
