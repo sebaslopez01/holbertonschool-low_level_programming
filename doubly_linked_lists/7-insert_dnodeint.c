801ea4038d8625d8e69c9caa1794d7c8f81321c7
Sebastian <sebaslopez.2002@gmail.com>
2022-08-03 15:14:34 -0700

    7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = *h;
 	if (idx == 0)
 	{
-		new_node->next = NULL;
+		new_node->next = current;
 		new_node->prev = NULL;
 		(*h)->prev = new_node;
 		*h = new_node;
