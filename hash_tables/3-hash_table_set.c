119c6b90adfeb82d7ebaf4690f5ee1ce7e931b72
Sebastian <sebaslopez.2002@gmail.com>
2022-08-31 09:42:48 -0700

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,6 +18,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
+	
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (!strcmp(current->key, key))
+		{
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
@@ -27,20 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_hash_node->value = strdup(value);
 
 	if (ht->array[index])
-	{
-		current = ht->array[index];
-		while (current != NULL)
-		{
-			if (!strcmp(current->key, key))
-			{
-				current->value = strdup(value);
-				return (1);
-			}
-			current = current->next;
-		}
-
 		new_hash_node->next = ht->array[index];
-	}
 	else
 		new_hash_node->next = NULL;
 
