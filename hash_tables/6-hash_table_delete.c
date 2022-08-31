6b1a475485983d08037093d80a500ce6f569f8ba
Sebastian <sebaslopez.2002@gmail.com>
2022-08-31 10:43:50 -0700

    6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table to delete
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current = NULL;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			current = ht->array[i];
+
+			ht->array[i] = current->next;
+
+			free(current->key);
+			free(current->value);
+			free(current);
+		}
+	}
+}
