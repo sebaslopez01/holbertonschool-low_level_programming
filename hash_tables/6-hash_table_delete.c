7c4c694a19fc8a9c46559074d387598722e864fa
Sebastian <sebaslopez.2002@gmail.com>
2022-08-31 10:46:38 -0700

    6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -21,7 +21,7 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			current = ht->array[i];
 
-			ht->array[i] = current->next;
+			ht->array[i] = ht->array[i]->next;
 
 			free(current->key);
 			free(current->value);
