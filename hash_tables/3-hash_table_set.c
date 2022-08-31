f8d071bb53a0a054e12d97bd1622d3b7306c8ca1
Sebastian <sebaslopez.2002@gmail.com>
2022-08-31 09:36:29 -0700

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -35,7 +35,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 			if (!strcmp(current->key, key))
 			{
-				free(current);
 				current = new_hash_node;
 				return (1);
 			}
