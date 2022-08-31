00a9e1ae00bb0b5fe2a9d2f17cf9265458b53ea7
Sebastian <sebaslopez.2002@gmail.com>
2022-08-31 08:33:01 -0700

    Betty styles

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash_table->size = size;
-	
+
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_hash_table->array)
 		return (NULL);
