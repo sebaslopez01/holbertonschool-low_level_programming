4144dfcc84e2a2b2c649d569b4b6de98d87d3256
Sebastian <sebaslopez.2002@gmail.com>
2022-09-08 17:49:33 -0700

    1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,75 @@
+#include "search_algos.h"
+
+
+/**
+ * print_array - Prints an array of integers
+ * @array: Array to print
+ * @size: Size of the array
+ *
+ * Return: void
+ */
+void print_array(int *array, size_t size)
+{
+	printf("Searching in array: ");
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+
+	printf("\n");
+}
+
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ * using the binary search algorithm
+ * @array: Array to search in
+ * @size: Size of the array
+ * @value: The value to search for
+ *
+ * Return: The index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int middle, left = 0, right = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	print_array(array, sizz);
+
+	while (left <= right)
+	{
+		middle = (int)((left + right) / 2);
+		printf("Searching in array: ");
+		if (array[middle] < value)
+		{
+			for (i = middle + 1; i < right; i++)
+			{
+				if (i > middle + 1)
+					printf(", ");
+				printf("%d", array[i]);
+			}
+			printf("\n");
+			left = middle + 1;
+		}
+		else if (array[middle] > value)
+		{
+			for (i = left; left < middle; i++)
+			{
+				if (i > left)
+					printf(", ");
+				printf("%d", array[i]);
+			}
+			printf("\n");
+			right = middle - 1;
+		}
+		else
+			return (middle);
+	}
+	return (-1);
+}
