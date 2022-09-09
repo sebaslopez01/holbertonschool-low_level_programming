25502084df545d1f40285c6443c698bbae017f9f
Sebastian <sebaslopez.2002@gmail.com>
2022-09-08 17:56:23 -0700

    1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -60,7 +60,7 @@ int binary_search(int *array, size_t size, int value)
 		}
 		else if (array[middle] > value)
 		{
-			for (i = left; left < middle; i++)
+			for (i = left; i < middle; i++)
 			{
 				if (i > left)
 					printf(", ");
