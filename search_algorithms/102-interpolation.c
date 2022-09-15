31d9b6337775aab2da29e649dd600bbf1057094d
Sebastian <sebaslopez.2002@gmail.com>
2022-09-15 09:42:29 -0700

    102

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+
+/**
+ * interpolation_search - Searches for a value in a sorted array of integers
+ * using the binary search algorithm
+ * @array: Array to search in
+ * @size: Size of the array
+ * @value: The value to search for
+ *
+ * Return: The index where value is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int pos, low = 0, high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+		if (pos > size - 1)
+		{
+			printf("Value checked array[%d] is out of range\n", pos);
+			return (-1);
+		}
+
+		printf("Value checked array[%d] = [%d]\n", pos, value);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			return (pos);
+	}
+
+	return (-1);
+}
